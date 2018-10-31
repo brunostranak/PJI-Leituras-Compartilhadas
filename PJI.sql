06e3196275ddd6eea426673205c8af7587ee4484
CamiloFernanda <41637116+CamiloFernanda@users.noreply.github.com>
2018-10-31 11:44:20 -0300

    Add files via upload

---
+++ PJI.sql
@@ -0,0 +1,40 @@
+DROP DATABASE PJI;
+CREATE DATABASE WEBLOJA;
+USE WEBLOJA;
+
+
+
+CREATE TABLE tblAlunos(
+idAlunos INT AUTO_INCREMENT,
+nomeAluno VARCHAR(60),
+
+email VARCHAR(60),
+prontuario VARCHAR(7),
+senha VARCHAR(20),
+PRIMARY KEY(idAlunos)
+) engine = innodb;
+
+CREATE TABLE tblObras(
+idObra INT AUTO_INCREMENT,
+descricao VARCHAR(60),
+nomeObra VARCHAR(60),
+autorObra VARCHAR(60),
+editoraObra VARCHAR(60),
+
+PRIMARY KEY (idObra)
+) engine = innodb;
+
+CREATE TABLE tblEmprestimos(
+idEmprestimo INT AUTO_INCREMENT,
+idObra INT,
+nomeDono VARCHAR(60),
+quemPegou VARCHAR(60),
+
+PRIMARY KEY(idEmprestimo),
+FOREIGN KEY (idObra) REFERENCES tblObras (idObra)
+
+
+) engine = innodb;
+
+
+
